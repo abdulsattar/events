d4843be12846663e66c55a05d3cb637fa2d85d2d
Abdulsattar Mohammed <asattar.md@gmail.com>
2016-11-06 23:21:13 +0530

    Add tests for session

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -5,4 +5,14 @@ defmodule SessionControllerTest do
     conn = get conn, "/login"
     assert html_response(conn, 200) =~ "Admin Login"
   end
+
+  test "POST /login with valid password", %{conn: conn} do
+    conn = post conn, "/login", ["password": "sattar"]
+    assert redirected_to(conn, 302) =~ "/admin"
+  end
+
+  test "POST /login with invalid password", %{conn: conn} do
+    conn = post conn, "/login", ["password": "invalid_password"]
+    assert html_response(conn, 200) =~ "Incorrect password."
+  end
 end
